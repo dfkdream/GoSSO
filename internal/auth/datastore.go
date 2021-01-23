e51741a71415ab4016027355cc48d8a41b78d29c
dfkdream <dfkdream@gmail.com>
2021-01-23 14:10:26 +0900

    Add auth/datastore

---
+++ internal/auth/datastore.go
@@ -0,0 +1,56 @@
+package auth
+
+import (
+	"github.com/asdine/storm/v3"
+	"github.com/asdine/storm/v3/codec/gob"
+	"github.com/google/uuid"
+)
+
+type DataStore struct {
+	db *storm.DB
+}
+
+func NewDataStore(path string) (*DataStore, error) {
+	db, err := storm.Open(path, storm.Codec(gob.Codec))
+	if err != nil {
+		return nil, err
+	}
+
+	return &DataStore{
+		db: db,
+	}, nil
+}
+
+func (d DataStore) AddUser(user *User) error {
+	if _, err := d.GetUserByID(user.ID); err == nil {
+		return storm.ErrAlreadyExists // prevent overriding
+	}
+
+	return d.db.Save(user)
+}
+
+func (d DataStore) UpdateUser(user *User) error {
+	return d.db.Update(user)
+}
+
+func (d DataStore) DeleteUser(user *User) error {
+	return d.db.DeleteStruct(user)
+}
+
+func (d DataStore) GetUserByID(id uuid.UUID) (*User, error) {
+	user := new(User)
+	err := d.db.One("ID", id, user)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (d DataStore) GetUserByUsername(username string) (*User, error) {
+	user := new(User)
+	err := d.db.One("Username", username, user)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
---
+++ internal/auth/datastore_test.go
@@ -0,0 +1,226 @@
+package auth
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"testing"
+
+	"github.com/asdine/storm/v3"
+
+	"github.com/dfkdream/GoSSO/internal/auth/secondfactor"
+	"github.com/dfkdream/permission"
+
+	"github.com/google/uuid"
+)
+
+func compareUser(u1, u2 User) bool {
+	return fmt.Sprintf("%+v", u1) == fmt.Sprintf("%+v", u2)
+}
+
+func createTempDS() *DataStore {
+	testDir, err := ioutil.TempDir("", "datastore")
+	//fmt.Println(testDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	d, err := NewDataStore(filepath.Join(testDir, "test.db"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return d
+}
+
+func mustHashPassword(password string) Password {
+	p, err := HashPassword(password)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return p
+}
+
+func TestNewDataStore(t *testing.T) {
+	createTempDS()
+}
+
+func TestDataStore_AddUser(t *testing.T) {
+	ds := createTempDS()
+
+	p, err := permission.FromString("+:sso")
+	if err != nil {
+		t.Error(err)
+	}
+
+	u1 := &User{
+		ID:            uuid.New(),
+		Username:      "hello",
+		Password:      mustHashPassword("world"),
+		SecondFactors: []secondfactor.SecondFactor{},
+		Permissions:   []permission.Permission{p},
+	}
+
+	err = ds.AddUser(u1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u1.Username = "hello-a"
+	err = ds.AddUser(u1)
+	if err == nil {
+		t.Error("Expected ErrAlreadyExists error but got nil")
+	}
+
+	u1.ID = uuid.New()
+	u1.Username = "hello"
+	err = ds.AddUser(u1)
+	if err != storm.ErrAlreadyExists {
+		t.Error("Expected ErrAlreadyExists error but got nil")
+	}
+}
+
+func TestDataStore_GetUserByID(t *testing.T) {
+	ds := createTempDS()
+
+	p, err := permission.FromString("+:sso")
+	if err != nil {
+		t.Error(err)
+	}
+
+	u1 := &User{
+		ID:            uuid.New(),
+		Username:      "hello",
+		Password:      mustHashPassword("world"),
+		SecondFactors: make([]secondfactor.SecondFactor, 0),
+		Permissions:   []permission.Permission{p},
+	}
+
+	err = ds.AddUser(u1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = ds.GetUserByID(uuid.New())
+	if err != storm.ErrNotFound {
+		t.Error(err)
+	}
+
+	u, err := ds.GetUserByID(u1.ID)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !compareUser(*u1, *u) {
+		t.Errorf("u1 (%+v) != u (%+v)", *u1, *u)
+	}
+}
+
+func TestDataStore_GetUserByUsername(t *testing.T) {
+	ds := createTempDS()
+
+	p, err := permission.FromString("+:sso")
+	if err != nil {
+		t.Error(err)
+	}
+
+	u1 := &User{
+		ID:            uuid.New(),
+		Username:      "hello",
+		Password:      mustHashPassword("world"),
+		SecondFactors: []secondfactor.SecondFactor{},
+		Permissions:   []permission.Permission{p},
+	}
+
+	err = ds.AddUser(u1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = ds.GetUserByUsername("hello-a")
+	if err != storm.ErrNotFound {
+		t.Error(err)
+	}
+
+	u, err := ds.GetUserByUsername(u1.Username)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !compareUser(*u1, *u) {
+		t.Errorf("u1 (%+v) != u (%+v)", *u1, *u)
+	}
+}
+
+func TestDataStore_UpdateUser(t *testing.T) {
+	ds := createTempDS()
+
+	p, err := permission.FromString("+:sso")
+	if err != nil {
+		t.Error(err)
+	}
+
+	u1 := &User{
+		ID:            uuid.New(),
+		Username:      "hello",
+		Password:      mustHashPassword("world"),
+		SecondFactors: []secondfactor.SecondFactor{},
+		Permissions:   []permission.Permission{p},
+	}
+
+	err = ds.AddUser(u1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u1.Username = "hola"
+	err = ds.UpdateUser(u1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = ds.GetUserByUsername("hello")
+	if err != storm.ErrNotFound {
+		t.Error(err)
+	}
+
+	u, err := ds.GetUserByID(u1.ID)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !compareUser(*u1, *u) {
+		t.Errorf("u1 (%+v) != u (%+v)", *u1, *u)
+	}
+}
+
+func TestDataStore_DeleteUser(t *testing.T) {
+	ds := createTempDS()
+
+	p, err := permission.FromString("+:sso")
+	if err != nil {
+		t.Error(err)
+	}
+
+	u1 := &User{
+		ID:            uuid.New(),
+		Username:      "hello",
+		Password:      mustHashPassword("world"),
+		SecondFactors: []secondfactor.SecondFactor{},
+		Permissions:   []permission.Permission{p},
+	}
+
+	err = ds.AddUser(u1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = ds.DeleteUser(u1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = ds.GetUserByID(u1.ID)
+	if err != storm.ErrNotFound {
+		t.Error(err)
+	}
+}
