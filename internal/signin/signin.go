67539e53e32b0f1e4b23ad826cd7e5e817dbc51e
dfkdream <dfkdream@gmail.com>
2021-02-02 08:12:06 +0900

    Add Sign In Handler

---
+++ internal/signin/signin.go
@@ -0,0 +1,135 @@
+// Sign in is the only way to generate refresh token
+// It does not use REST API to store refresh token as HTTPOnly Cookie for Cross-Site Access Token request
+package signin
+
+import (
+	"crypto/ecdsa"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/dgrijalva/jwt-go"
+
+	"github.com/dfkdream/permission"
+
+	"github.com/dfkdream/GoSSO/internal/auth"
+)
+
+var refreshTokenPermissions = []permission.Permission{
+	{
+		Allow:      true,
+		Namespaces: []string{"gosso", "token", "refresh"},
+	},
+}
+
+var defaultPermissions = []permission.Permission{
+	{
+		Allow:      true,
+		Namespaces: []string{"gosso"},
+	},
+}
+
+type Handler struct {
+	ds                  *auth.DataStore
+	pk                  *ecdsa.PrivateKey
+	refreshTokenTimeout time.Duration
+}
+
+func New(dataStore *auth.DataStore, privateKey *ecdsa.PrivateKey, tokenTimeout time.Duration) Handler {
+	return Handler{
+		ds:                  dataStore,
+		pk:                  privateKey,
+		refreshTokenTimeout: tokenTimeout,
+	}
+}
+
+func (h Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	redirect := req.Referer()
+
+	if req.Method != "POST" {
+		http.Redirect(res, req, redirect, http.StatusTemporaryRedirect)
+		return
+	}
+
+	err := req.ParseForm()
+	if err != nil {
+		http.Redirect(res, req, redirect, http.StatusTemporaryRedirect)
+		return
+	}
+
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+
+	if username == "" || password == "" {
+		http.Redirect(res, req, redirect, http.StatusTemporaryRedirect)
+		return
+	}
+
+	// Create admin account with default permissions if user not exists
+	if h.ds.Size() == 0 {
+		pw, err := auth.HashPassword(password)
+		if err != nil {
+			http.Redirect(res, req, redirect, http.StatusTemporaryRedirect)
+			return
+		}
+
+		err = h.ds.AddUser(&auth.User{
+			ID:          uuid.New(),
+			Username:    username,
+			Password:    pw,
+			Permissions: defaultPermissions,
+		})
+		if err != nil {
+			http.Redirect(res, req, redirect, http.StatusTemporaryRedirect)
+			return
+		}
+	}
+
+	u, err := h.ds.GetUserByUsername(username)
+	if err != nil {
+		http.Redirect(res, req, redirect, http.StatusTemporaryRedirect)
+		return
+	}
+
+	if u.Password.Validate(password) {
+
+		if r := req.FormValue("redirect"); r != "" {
+			redirect = r
+		} else {
+			redirect = "/"
+		}
+
+		token, err := h.refreshToken(u)
+		if err != nil {
+			http.Redirect(res, req, redirect, http.StatusTemporaryRedirect)
+			return
+		}
+
+		http.SetCookie(res, &http.Cookie{
+			Name:     "token",
+			Value:    token,
+			Path:     "",
+			Domain:   "",
+			Secure:   true,
+			HttpOnly: true,
+			SameSite: 0,
+		})
+
+		http.Redirect(res, req, redirect, http.StatusTemporaryRedirect)
+	} else {
+		http.Redirect(res, req, redirect, http.StatusTemporaryRedirect)
+	}
+}
+
+func (h Handler) refreshToken(u *auth.User) (string, error) {
+	payload := u
+	payload.Permissions = refreshTokenPermissions
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+		"iss": "gosso",
+		"nbf": time.Now().Unix(),
+		"exp": time.Now().Add(h.refreshTokenTimeout).Unix(),
+		"usr": payload,
+	})
+	return token.SignedString(h.pk)
+}
---
+++ internal/signin/signin_test.go
@@ -0,0 +1,124 @@
+package signin
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http/httptest"
+	"net/url"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+
+	"github.com/dfkdream/GoSSO/internal/auth"
+)
+
+func createTempDS() *auth.DataStore {
+	testDir, err := ioutil.TempDir("", "datastore")
+	//fmt.Println(testDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	d, err := auth.NewDataStore(filepath.Join(testDir, "test.db"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return d
+}
+
+func TestHandler_ServeHTTP(t *testing.T) {
+	pk, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ds := createTempDS()
+
+	h := New(ds, pk, time.Hour)
+
+	// Scenario 01 : Initialize User
+	{
+		data := url.Values{}
+		data.Set("username", "hello")
+		data.Add("password", "world")
+
+		req := httptest.NewRequest("POST", "/", bytes.NewBufferString(data.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+
+		res := httptest.NewRecorder()
+
+		h.ServeHTTP(res, req)
+
+		tok := res.Result().Cookies()[0].Value
+
+		token, err := jwt.Parse(tok, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return pk.Public(), nil
+		})
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		if !token.Valid {
+			t.Error("token not valid")
+		}
+	}
+
+	// Scenario 02: Invalid Sign in attempt
+	{
+		data := url.Values{}
+		data.Set("username", "halo")
+		data.Add("password", "world")
+
+		req := httptest.NewRequest("POST", "/", bytes.NewBufferString(data.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+
+		res := httptest.NewRecorder()
+
+		h.ServeHTTP(res, req)
+
+		if len(res.Result().Cookies()) > 0 {
+			t.Errorf("Expected no cookie but got %+v", res.Result().Cookies())
+		}
+	}
+
+	// Scenario 03 : Valid Sign in attempt
+	{
+		data := url.Values{}
+		data.Set("username", "hello")
+		data.Add("password", "world")
+
+		req := httptest.NewRequest("POST", "/", bytes.NewBufferString(data.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+
+		res := httptest.NewRecorder()
+
+		h.ServeHTTP(res, req)
+
+		tok := res.Result().Cookies()[0].Value
+
+		token, err := jwt.Parse(tok, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return pk.Public(), nil
+		})
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		if !token.Valid {
+			t.Error("token not valid")
+		}
+	}
+}
